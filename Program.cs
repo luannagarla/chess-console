72e08d5b905c6510b92b08c1c7e1dc1a1f4730e2
Luanna Garla <luagarla@gmail.com>
2021-03-11 11:45:34 -0300

    Implementation

--- Program.cs
+++ Program.cs
@@ -8,11 +8,25 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
+            try
+            {
+                GameBoard board = new GameBoard(8,8);
+            
+                board.putPiece(new Rook(board, Color.Black), new Position(0,0));
+                board.putPiece(new Rook(board, Color.Black), new Position(1,3));
+                board.putPiece(new King(board, Color.Black), new Position(0,2));
 
-         PositionChess pos = new PositionChess('c',7);   
-         Console.WriteLine(pos);
+                board.putPiece(new Rook(board, Color.White), new Position(3,5));
 
-         Console.WriteLine(pos.toPosition());
+
+                Screen.printGameBoard(board);
+
+
+            }
+            catch (BoardException e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
             
         }
--- Screen.cs
+++ Screen.cs
@@ -10,21 +10,38 @@ namespace xadrez_console
         {
             for(int i = 0; i < board.lines; i++)
             {
+                Console.Write(8 - i + " ");
+
                 for(int j = 0; j < board.columns; j++)
-                {   
+                {                       
                     if (board.piece(i,j) == null)
                     {
                         Console.Write("- ");
                     }
                     else
                     {
-                        Console.Write(board.piece(i,j) +" ");
+                        printPiece(board.piece(i,j));
+                        Console.Write(" ");
                     }                                 
                 }
                 Console.WriteLine();
             }
-            
+            Console.WriteLine("  a b c d e f g h");
         }
 
+        public static void printPiece(Piece piece)
+        {
+           if (piece.color == Color.White)
+           {
+               Console.Write(piece);
+           }
+           else
+           {
+               ConsoleColor aux = Console.ForegroundColor;
+               Console.ForegroundColor = ConsoleColor.Yellow;
+               Console.Write(piece);
+               Console.ForegroundColor = aux;
+           }
+        }
     }
 }
